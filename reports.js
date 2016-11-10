14f1234e2a29b96d81709f4e0a837bf3fcf16d9a
Thomas Mery <thomas.mery@gmail.com>
2016-11-10 12:33:11 +0100

    Include reports js file

---
+++ reports.js
@@ -0,0 +1,1 @@
+alert('reports.js');
---
+++ reports.php
@@ -0,0 +1,22 @@
+<?php
+
+// adding to Menu
+function reports_pos_menu_item($menu) {
+
+    array_push($menu, array(
+        'id'     => 'wcpos-reports',  // any unique id 
+        'label'  => 'Reports',  // menu 
+        'href'   => '#reports'  // routing trigger
+    ));
+
+    return $menu;
+
+}
+add_filter('woocommerce_pos_menu', 'reports_pos_menu_item');
+
+// add js reports file
+function reports_pos_js_file($js){
+    $js['reports'] = get_template_directory_uri() . '/reports.js';
+    return $js;
+}
+add_filter('woocommerce_pos_enqueue_footer_js', 'reports_pos_js_file');
