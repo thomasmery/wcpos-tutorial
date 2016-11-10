746d7376d78d62fb2dde14bee69e9657c1b6f7f9
Thomas Mery <thomas.mery@gmail.com>
2016-11-10 13:00:45 +0100

    Add first page example

--- reports.js
+++ reports.js
@@ -1,1 +1,28 @@
-alert('reports.js');
+(function (app, $, _, bb) {
+	
+	// create a new Marionette View
+	var ReportsView = app.ItemView.extend({
+		template: function(){
+		  return 'Hello Paul!';
+		}
+	});
+
+	var ReportsRoute = app.Route.extend({
+		render: function(){
+		  var container = app.layout.getRegion('main');
+		  var view = new ReportsView();
+		  container.show(view);
+		}
+	});
+
+	var ReportsRouter = app.Router.extend({
+		routes: {
+		  'reports' : function(){
+		    return new ReportsRoute();
+		  }
+		}
+	});
+
+	new ReportsRouter();
+
+})(POS, jQuery, _, Backbone);
