99c00cf45a592a7b59ffa9d55827fb59d1372dd1
Thomas Mery <thomas.mery@gmail.com>
2016-11-11 12:35:37 +0100

    Update js enqueue url

--- reports.php
+++ reports.php
@@ -16,7 +16,7 @@ add_filter('woocommerce_pos_menu', 'reports_pos_menu_item');
 
 // add js reports file
 function reports_pos_js_file($js){
-    $js['reports'] = get_template_directory_uri() . '/reports.js';
+    $js['reports'] = get_template_directory_uri() . '/' . basename(__DIR__) . '/reports.js';
     return $js;
 }
 add_filter('woocommerce_pos_enqueue_footer_js', 'reports_pos_js_file');
